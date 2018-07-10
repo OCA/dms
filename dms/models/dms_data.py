d7f7d012a9b45744d034ad5869fee2c4130a56c3
Mathias Markl <mathias.markl@mukit.at>
2018-07-10 17:59:44 +0200

    Documents 2.0

--- dms/models/dms_data.py
+++ dms/models/dms_data.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 ###################################################################################
 # 
 #    Copyright (C) 2017 MuK IT GmbH
@@ -19,58 +17,25 @@
 #
 ###################################################################################
 
-import abc
 import logging
 
-from odoo import _
-from odoo import models, api, fields
-from odoo.exceptions import ValidationError, AccessError
+from odoo import models, api
 
 _logger = logging.getLogger(__name__)
 
 class DataModel(models.AbstractModel):
+    
     _name = 'muk_dms.data'
     _description = 'Base Data Model'
     
-    @abc.abstractmethod
+    @api.model
     def type(self):
         """Returns the data type."""
     
-    @abc.abstractmethod
+    @api.multi
     def content(self):
         """Returns the data object."""
-        
-    @abc.abstractmethod
+    
+    @api.multi
     def update(self, values):
         """Updated the data object."""
-        
-class DatabaseDataModel(models.Model):
-    _name = 'muk_dms.data_database'
-    _description = 'Database Data Model'
-    
-    _inherit = 'muk_dms.data'
-    
-    #----------------------------------------------------------
-    # Database
-    #----------------------------------------------------------
-    
-    data = fields.Binary(
-        string="Content")
-    
-    #----------------------------------------------------------
-    # Abstract Implementation
-    #----------------------------------------------------------
-    
-    def type(self):
-        return "database"
-    
-    def content(self):
-        return self.data
-    
-    def update(self, values):
-        if 'content' in values:
-            self.data = values['content']
-    
-    def delete(self):
-        self.data = None
-    
