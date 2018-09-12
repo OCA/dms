67e0d8f94a1ca340847452f3b492e5d11962ec83
Mathias Markl <mathias.markl@mukit.at>
2018-09-12 11:54:42 +0200

    v12

--- dms/models/__init__.py
+++ dms/models/__init__.py
@@ -1,31 +1,35 @@
 ###################################################################################
-# 
-#    Copyright (C) 2017 MuK IT GmbH
+#
+#    Copyright (c) 2017-2019 MuK IT GmbH.
+#
+#    This file is part of MuK Documents 
+#    (see https://mukit.at).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU Lesser General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###################################################################################
 
-from . import dms_model
-from . import dms_locking
-from . import dms_access
-from . import dms_settings
-from . import dms_directory
-from . import dms_file
-from . import dms_data
-from . import dms_tag
-from . import dms_category
-from . import dms_data_database
+from . import mixins_thumbnail
+
+from . import storage
+from . import directory
+from . import file
+
+from . import category
+from . import tag
+
 from . import ir_http
+from . import res_company
 from . import res_config_settings
+ 
