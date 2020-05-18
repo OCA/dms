c120bdd6dfc6f289d80c8081104738d25a484777
Enric Tobella <etobella@creublanca.es>
2020-05-18 12:00:06 +0200

    [IMP] : black, isort, prettier

--- dms/models/category.py
+++ dms/models/category.py
@@ -2,7 +2,7 @@
 #
 #    Copyright (c) 2017-2019 MuK IT GmbH.
 #
-#    This file is part of MuK Documents 
+#    This file is part of MuK Documents
 #    (see https://mukit.at).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -22,130 +22,125 @@
 
 import logging
 
-from odoo import models, api, fields
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
+
 class Category(models.Model):
-    
-    _name = 'muk_dms.category'
+
+    _name = "muk_dms.category"
     _description = "Document Category"
-    
+
     _inherit = [
-        'muk_utils.mixins.hierarchy',
+        "muk_utils.mixins.hierarchy",
     ]
-    
+
     _order = "name asc"
-    
+
     _parent_store = True
     _parent_name = "parent_category"
-    
+
     _parent_path_sudo = False
     _parent_path_store = True
-    
+
     _name_path_context = "dms_category_show_path"
-    
-    #----------------------------------------------------------
+
+    # ----------------------------------------------------------
     # Database
-    #----------------------------------------------------------
-    
-    name = fields.Char(
-        string='Name', 
-        required=True, 
-        translate=True)
-    
+    # ----------------------------------------------------------
+
+    name = fields.Char(string="Name", required=True, translate=True)
+
     active = fields.Boolean(
-        default=True, 
-        help="The active field allows you to hide the category without removing it.")
-    
+        default=True,
+        help="The active field allows you to hide the category without removing it.",
+    )
+
     parent_category = fields.Many2one(
-        comodel_name='muk_dms.category', 
+        comodel_name="muk_dms.category",
         context="{'dms_category_show_path': True}",
-        string='Parent Category',
-        ondelete='cascade',
-        index=True)
-    
+        string="Parent Category",
+        ondelete="cascade",
+        index=True,
+    )
+
     child_categories = fields.One2many(
-        comodel_name='muk_dms.category', 
-        inverse_name='parent_category',
-        string='Child Categories')
-    
-    parent_path = fields.Char(
-        string="Parent Path", 
-        index=True)
-    
+        comodel_name="muk_dms.category",
+        inverse_name="parent_category",
+        string="Child Categories",
+    )
+
+    parent_path = fields.Char(string="Parent Path", index=True)
+
     tags = fields.One2many(
-        comodel_name='muk_dms.tag', 
-        inverse_name='category',
-        string='Tags')
-    
+        comodel_name="muk_dms.tag", inverse_name="category", string="Tags"
+    )
+
     directories = fields.One2many(
-        comodel_name='muk_dms.directory', 
-        inverse_name='category',
-        string='Directories',
-        readonly=True)
-    
+        comodel_name="muk_dms.directory",
+        inverse_name="category",
+        string="Directories",
+        readonly=True,
+    )
+
     files = fields.One2many(
-        comodel_name='muk_dms.file', 
-        inverse_name='category',
-        string='Files',
-        readonly=True)
-    
+        comodel_name="muk_dms.file",
+        inverse_name="category",
+        string="Files",
+        readonly=True,
+    )
+
     count_categories = fields.Integer(
-        compute='_compute_count_categories',
-        string="Count Subcategories")
-    
-    count_tags = fields.Integer(
-        compute='_compute_count_tags',
-        string="Count Tags")
-    
+        compute="_compute_count_categories", string="Count Subcategories"
+    )
+
+    count_tags = fields.Integer(compute="_compute_count_tags", string="Count Tags")
+
     count_directories = fields.Integer(
-        compute='_compute_count_directories',
-        string="Count Directories")
-    
-    count_files = fields.Integer(
-        compute='_compute_count_files',
-        string="Count Files")
-    
-    #----------------------------------------------------------
+        compute="_compute_count_directories", string="Count Directories"
+    )
+
+    count_files = fields.Integer(compute="_compute_count_files", string="Count Files")
+
+    # ----------------------------------------------------------
     # Constrains
-    #----------------------------------------------------------
-    
+    # ----------------------------------------------------------
+
     _sql_constraints = [
-        ('name_uniq', 'unique (name)', "Category name already exists!"),
+        ("name_uniq", "unique (name)", "Category name already exists!"),
     ]
-       
-    #----------------------------------------------------------
+
+    # ----------------------------------------------------------
     # Read
-    #----------------------------------------------------------
-    
-    @api.depends('child_categories')
+    # ----------------------------------------------------------
+
+    @api.depends("child_categories")
     def _compute_count_categories(self):
         for record in self:
             record.count_categories = len(record.child_categories)
-    
-    @api.depends('tags')
+
+    @api.depends("tags")
     def _compute_count_tags(self):
         for record in self:
             record.count_tags = len(record.tags)
-    
-    @api.depends('directories')
+
+    @api.depends("directories")
     def _compute_count_directories(self):
         for record in self:
             record.count_directories = len(record.directories)
-    
-    @api.depends('files')
+
+    @api.depends("files")
     def _compute_count_files(self):
         for record in self:
             record.count_files = len(record.files)
-    
-    #----------------------------------------------------------
+
+    # ----------------------------------------------------------
     # Create
-    #----------------------------------------------------------
-    
-    @api.constrains('parent_category')
+    # ----------------------------------------------------------
+
+    @api.constrains("parent_category")
     def _check_category_recursion(self):
         if not self._check_recursion():
-            raise ValidationError(_('Error! You cannot create recursive categories.'))
+            raise ValidationError(_("Error! You cannot create recursive categories."))
         return True
-    
--- dms/models/ir_http.py
+++ dms/models/ir_http.py
@@ -2,7 +2,7 @@
 #
 #    Copyright (c) 2017-2019 MuK IT GmbH.
 #
-#    This file is part of MuK Documents 
+#    This file is part of MuK Documents
 #    (see https://mukit.at).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -26,37 +26,73 @@ import mimetypes
 
 from odoo import models
 from odoo.exceptions import AccessError
-from odoo.http import request, STATIC_CACHE
+from odoo.http import STATIC_CACHE, request
 from odoo.tools.mimetypes import guess_mimetype
 
 _logger = logging.getLogger(__name__)
 
+
 class IrHttp(models.AbstractModel):
-    
-    _inherit = 'ir.http'
-    
+
+    _inherit = "ir.http"
+
     @classmethod
-    def binary_content(cls, xmlid=None, model='ir.attachment', id=None, field='datas', unique=False, filename=None,
-        filename_field='datas_fname', download=False, mimetype=None, default_mimetype='application/octet-stream',
-        access_token=None, related_id=None, access_mode=None, env=None):
+    def binary_content(
+        cls,
+        xmlid=None,
+        model="ir.attachment",
+        id=None,
+        field="datas",
+        unique=False,
+        filename=None,
+        filename_field="datas_fname",
+        download=False,
+        mimetype=None,
+        default_mimetype="application/octet-stream",
+        access_token=None,
+        related_id=None,
+        access_mode=None,
+        env=None,
+    ):
         res_status, res_headers, res_content = super(IrHttp, cls).binary_content(
-            xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename, mimetype=mimetype,
-            filename_field=filename_field, download=download, access_mode=access_mode, related_id=related_id,
-            default_mimetype=default_mimetype, access_token=access_token, env=env)
+            xmlid=xmlid,
+            model=model,
+            id=id,
+            field=field,
+            unique=unique,
+            filename=filename,
+            mimetype=mimetype,
+            filename_field=filename_field,
+            download=download,
+            access_mode=access_mode,
+            related_id=related_id,
+            default_mimetype=default_mimetype,
+            access_token=access_token,
+            env=env,
+        )
         if res_status == 200:
             env = env or request.env
-            if model == "muk_dms.file" and field != 'content':
-                obj = cls._xmlid_to_obj(env, xmlid) if xmlid else env[model].browse(int(id))
-                filename = obj[filename_field] if not filename and filename_field in obj else filename
+            if model == "muk_dms.file" and field != "content":
+                obj = (
+                    cls._xmlid_to_obj(env, xmlid)
+                    if xmlid
+                    else env[model].browse(int(id))
+                )
+                filename = (
+                    obj[filename_field]
+                    if not filename and filename_field in obj
+                    else filename
+                )
                 mimetype = filename and mimetypes.guess_type(filename)[0]
                 if not mimetype:
-                    mimetype = guess_mimetype(base64.b64decode(res_content), default=default_mimetype)
+                    mimetype = guess_mimetype(
+                        base64.b64decode(res_content), default=default_mimetype
+                    )
                 headers = []
                 for header in res_headers:
-                    if header[0] == 'Content-Type':
-                        headers.append(('Content-Type', mimetype))
+                    if header[0] == "Content-Type":
+                        headers.append(("Content-Type", mimetype))
                     else:
                         headers.append(header)
                 return res_status, headers, res_content
         return res_status, res_headers, res_content
-        
