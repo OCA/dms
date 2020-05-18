01587273e10357d952096915446a2720d2443d5c
Enric Tobella <etobella@creublanca.es>
2020-05-18 12:43:13 +0200

    [MIG][13.0] dms: Finish migration

--- dms/models/res_company.py
+++ dms/models/res_company.py
@@ -1,24 +1,6 @@
-###################################################################################
-#
-#    Copyright (c) 2017-2019 MuK IT GmbH.
-#
-#    This file is part of MuK Documents
-#    (see https://mukit.at).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-###################################################################################
+# Copyright 2020 Creu Blanca
+# Copyright 2017-2019 MuK IT GmbH
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import logging
 
@@ -105,15 +87,13 @@ class ResCompany(models.Model):
 
     @api.model
     def action_open_documents_onboarding_directory(self):
-        storage = self.env["dms.storage"].search(
-            [], order="create_date desc", limit=1
-        )
+        storage = self.env["dms.storage"].search([], order="create_date desc", limit=1)
         action = self.env.ref("dms.action_dms_directory_new").read()[0]
         action["context"] = {
             **self.env.context,
             **{
                 "default_is_root_directory": True,
-                "default_root_storage": storage and storage.id,
+                "default_root_storage_id": storage and storage.id,
             },
         }
         return action
--- dms/models/storage.py
+++ dms/models/storage.py
@@ -1,27 +1,8 @@
-###################################################################################
-#
-#    Copyright (c) 2017-2019 MuK IT GmbH.
-#
-#    This file is part of MuK Documents
-#    (see https://mukit.at).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-###################################################################################
+# Copyright 2017-2019 MuK IT GmbH.
+# Copyright 2020 Creu Blanca
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import logging
-import textwrap
 
 from odoo import _, api, fields, models
 from odoo.exceptions import AccessError
@@ -45,15 +26,17 @@ class Storage(models.Model):
         string="Save Type",
         default="database",
         required=True,
-        help="""The save type is used to determine how a file is saved by the system. If you change
-            this setting, you can migrate existing files manually by triggering the action.""",
+        help="""The save type is used to determine how a file is saved by the
+        system. If you change this setting, you can migrate existing files
+        manually by triggering the action.""",
     )
 
-    company = fields.Many2one(
+    company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
         default=lambda self: self.env.user.company_id,
-        help="If set, directories and files will only be available for the selected company.",
+        help="If set, directories and files will only be available for "
+        "the selected company.",
     )
 
     is_hidden = fields.Boolean(
@@ -62,27 +45,27 @@ class Storage(models.Model):
         help="Indicates if directories and files are hidden by default.",
     )
 
-    root_directories = fields.One2many(
+    root_directory_ids = fields.One2many(
         comodel_name="dms.directory",
-        inverse_name="root_storage",
+        inverse_name="root_storage_id",
         string="Root Directories",
         auto_join=False,
         readonly=False,
         copy=False,
     )
 
-    storage_directories = fields.One2many(
+    storage_directory_ids = fields.One2many(
         comodel_name="dms.directory",
-        inverse_name="storage",
+        inverse_name="storage_id",
         string="Directories",
         auto_join=False,
         readonly=True,
         copy=False,
     )
 
-    storage_files = fields.One2many(
+    storage_file_ids = fields.One2many(
         comodel_name="dms.file",
-        inverse_name="storage",
+        inverse_name="storage_id",
         string="Files",
         auto_join=False,
         readonly=True,
@@ -101,17 +84,19 @@ class Storage(models.Model):
     # Actions
     # ----------------------------------------------------------
 
-    @api.multi
     def action_storage_migrate(self):
         if not self.env.user.has_group("dms.group_dms_manager"):
             raise AccessError(_("Only managers can execute this action."))
         files = self.env["dms.file"].with_context(active_test=False).sudo()
         for record in self:
-            domain = ["&", ("content_binary", "=", False), ("storage", "=", record.id)]
+            domain = [
+                "&",
+                ("content_binary", "=", False),
+                ("storage_id", "=", record.id),
+            ]
             files |= files.search(domain)
         files.action_migrate()
 
-    @api.multi
     def action_save_onboarding_storage_step(self):
         self.env.user.company_id.set_onboarding_step_done(
             "documents_onboarding_storage_state"
@@ -121,12 +106,12 @@ class Storage(models.Model):
     # Read, View
     # ----------------------------------------------------------
 
-    @api.depends("storage_directories")
+    @api.depends("storage_directory_ids")
     def _compute_count_storage_directories(self):
         for record in self:
-            record.count_storage_directories = len(record.storage_directories)
+            record.count_storage_directories = len(record.storage_directory_ids)
 
-    @api.depends("storage_files")
+    @api.depends("storage_file_ids")
     def _compute_count_storage_files(self):
         for record in self:
-            record.count_storage_files = len(record.storage_files)
+            record.count_storage_files = len(record.storage_file_ids)
