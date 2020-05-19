216d201cc2af990eb9716460cf3818aaa4d88350
Enric Tobella <etobella@creublanca.es>
2020-05-19 10:24:56 +0200

    [MIG] dms: fix tests

--- dms/tests/test_file.py
+++ dms/tests/test_file.py
@@ -2,13 +2,7 @@
 # Copyright 2020 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import logging
-import os
-
-from .common import DocumentsBaseCase, multi_users, setup_data_function
-
-_path = os.path.dirname(os.path.dirname(__file__))
-_logger = logging.getLogger(__name__)
+from .common import DocumentsBaseCase, multi_users
 
 
 class FileTestCase(DocumentsBaseCase):
@@ -34,8 +28,7 @@ class FileTestCase(DocumentsBaseCase):
         )
         self.new_file_sub_directory = self.create_file(directory=self.new_sub_directory)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_create_file(self):
         root_directory = self.create_directory(storage=self.new_storage)
         self.create_file(root_directory)
@@ -44,8 +37,7 @@ class FileTestCase(DocumentsBaseCase):
         self.assertEqual(root_directory.count_total_files, 2)
         self.assertEqual(sub_directory.count_files, 1)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_lock_file(self):
         file = self.create_file(with_user=True)
         file.with_user(self.env.uid).lock()
@@ -53,33 +45,30 @@ class FileTestCase(DocumentsBaseCase):
         file.with_user(self.env.uid).unlock()
         self.assertFalse(file.is_locked)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_copy_file(self):
         copy_file = self.file_demo_01.copy()
         self.assertEqual(self.file_demo_01.storage_id, copy_file.storage_id)
         self.assertEqual(self.file_demo_01.content, copy_file.content)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_rename_file(self):
         file = self.create_file(with_user=True)
         extension = file.extension
         file.with_user(self.env.uid).write({"name": "Test.jpg"})
         self.assertNotEqual(file.extension, extension)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_move_file(self):
         path_names = self.file_demo_01.path_names
         self.file_demo_01.write({"directory_id": self.directory_root_demo_01.id})
         self.file_demo_01.flush()
         self.assertNotEqual(path_names, self.file_demo_01.path_names)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_move_directory(self):
-        path_names = self.directory_root_demo_03.file_ids[0].path_names
+        file = self.directory_root_demo_03.file_ids[0]
+        path_names = file.path_names
         self.directory_root_demo_01.write(
             {
                 "root_storage_id": False,
@@ -87,76 +76,64 @@ class FileTestCase(DocumentsBaseCase):
                 "parent_id": self.directory_root_demo_02.id,
             }
         )
-        self.assertNotEqual(
-            path_names, self.directory_root_demo_03.file_ids[0].path_names
-        )
+        self.directory_root_demo_01.flush()
+        # We need to refresh as the field is not stored
+        file.refresh()
+        self.assertNotEqual(path_names, file.path_names)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_unlink_file(self):
         file = self.create_file(with_user=True)
         file.with_user(self.env.uid).unlink()
         self.assertFalse(file.exists())
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_thumbnail(self):
         self.assertTrue(self.file_demo_01.thumbnail)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_path_names(self):
         self.assertTrue(self.file_demo_01.path_names)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_path_json(self):
         self.assertTrue(self.file_demo_01.path_json)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_mimetype(self):
         self.assertTrue(self.file_demo_01.mimetype)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_extension(self):
         self.assertTrue(self.file_demo_01.extension)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_size_calculation(self):
         self.assertTrue(self.file_demo_01.size)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_checksum_calculation(self):
         self.assertTrue(self.file_demo_01.checksum)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_content_bin_size(self):
         file = self.file_demo_01.with_context(bin_size=True)
         self.assertTrue(file.content)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_content_base64(self):
         file = self.file_demo_01.with_context(bin_size=False)
         self.assertTrue(file.content)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_save_type(self):
         self.assertTrue(self.file_demo_01.save_type)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_compute_migration(self):
         self.assertTrue(self.file_demo_01.migration)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_search_panel(self):
         self.assertTrue(self.file.search_panel_select_range("directory_id"))
         self.assertTrue(self.file.search_panel_select_multi_range("directory_id"))
--- dms/tests/test_storage.py
+++ dms/tests/test_storage.py
@@ -2,31 +2,21 @@
 # Copyright 2020 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import logging
-import os
-
-from .common import DocumentsBaseCase, multi_users, setup_data_function
-
-_path = os.path.dirname(os.path.dirname(__file__))
-_logger = logging.getLogger(__name__)
+from .common import DocumentsBaseCase, multi_users
 
 
 class StorageTestCase(DocumentsBaseCase):
     def _setup_test_data(self):
-        super(StorageTestCase, self)._setup_test_data()
         self.storage_demo = self.browse_ref("dms.storage_demo")
 
-    @multi_users(lambda self: self.multi_users(demo=False))
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(demo=False), callback="_setup_test_data")
     def test_action_storage_migrate(self):
-        self.storage_demo.action_storage_migrate()
+        self.storage_demo.with_user(self.uid).action_storage_migrate()
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_count_storage_directories(self):
         self.assertTrue(self.storage_demo.count_storage_directories)
 
-    @multi_users(lambda self: self.multi_users())
-    @setup_data_function(setup_func="_setup_test_data")
+    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
     def test_count_storage_files(self):
         self.assertTrue(self.storage_demo.count_storage_files)
