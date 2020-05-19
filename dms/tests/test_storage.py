20985c6610583ed6251a917f85c2e9697977242a
Enric Tobella <etobella@creublanca.es>
2020-05-19 13:29:17 +0200

    [MIG] dms: File functionality

--- dms/tests/test_storage.py
+++ dms/tests/test_storage.py
@@ -1,22 +1,39 @@
 # Copyright 2017-2019 MuK IT GmbH.
 # Copyright 2020 Creu Blanca
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
-from .common import DocumentsBaseCase, multi_users
+from .common import multi_users
+from .test_storage_database import StorageTestCase
 
 
-class StorageTestCase(DocumentsBaseCase):
-    def _setup_test_data(self):
-        self.storage_demo = self.browse_ref("dms.storage_demo")
-
+class StorageLObjectTestCase(StorageTestCase):
     @multi_users(lambda self: self.multi_users(demo=False), callback="_setup_test_data")
-    def test_action_storage_migrate(self):
-        self.storage_demo.with_user(self.uid).action_storage_migrate()
-
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
-    def test_count_storage_directories(self):
-        self.assertTrue(self.storage_demo.count_storage_directories)
-
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
-    def test_count_storage_files(self):
-        self.assertTrue(self.storage_demo.count_storage_files)
+    def test_file_migrate(self):
+        storage = self.create_storage(sudo=False).with_user(self.uid)
+        file_01 = self.create_file(storage=storage)
+        self.assertTrue(file_01.storage_id.id == storage.id)
+        self.assertTrue(file_01.storage_id.save_type == "database")
+        self.assertTrue(file_01.save_type == "database")
+        storage.write({"save_type": "file"})
+        file_02 = self.create_file(storage=storage)
+        self.assertTrue(file_01.storage_id.id == storage.id)
+        self.assertTrue(file_01.storage_id.save_type == "file")
+        self.assertTrue(file_01.save_type == "database")
+        self.assertTrue(file_02.storage_id.id == storage.id)
+        self.assertTrue(file_02.storage_id.save_type == "file")
+        self.assertTrue(file_02.save_type == "file")
+        storage.action_storage_migrate()
+        self.assertTrue(file_01.storage_id.id == storage.id)
+        self.assertTrue(file_01.storage_id.save_type == "file")
+        self.assertTrue(file_01.save_type == "file")
+        self.assertTrue(file_02.storage_id.id == storage.id)
+        self.assertTrue(file_02.storage_id.save_type == "file")
+        self.assertTrue(file_02.save_type == "file")
+        storage.write({"save_type": "database"})
+        storage.action_storage_migrate()
+        self.assertTrue(file_01.storage_id.id == storage.id)
+        self.assertTrue(file_01.storage_id.save_type == "database")
+        self.assertTrue(file_01.save_type == "database")
+        self.assertTrue(file_02.storage_id.id == storage.id)
+        self.assertTrue(file_02.storage_id.save_type == "database")
+        self.assertTrue(file_02.save_type == "database")
