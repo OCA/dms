894d5af1b9e99cfbd72f23b8fe0e6805577f78c6
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-08-10 12:59:04 +0200

    [12.0][ADD] dms_field: New view

---
+++ dms_field/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "DMS Field",
+    "summary": """
+        Summary""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/dms",
+    "depends": ["dms"],
+    "data": ["templates/assets.xml", "views/dms_storage.xml"],
+    "qweb": ["static/src/xml/*.xml"],
+}
