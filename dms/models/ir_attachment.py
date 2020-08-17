5b724de1e7233490669ee998f72e9b4e36ee697a
Luis Malta <luis.malta@kmee.com.br>
2020-08-17 09:46:03 -0300

    [ADD] Handle attachment with same name

--- dms/models/ir_attachment.py
+++ dms/models/ir_attachment.py
@@ -1,9 +1,23 @@
+import re
+
 from odoo import api, models, fields, tools
 
 
 class IrAttachment(models.Model):
 
     _inherit = "ir.attachment"
+
+    def _dms_file_name(self, directory_id, file_name):
+        count = 1
+        for file in directory_id.file_ids:
+
+            dms_file_name = re.sub(r' \([0-9]*\)', '', file.name)
+            if dms_file_name == file_name:
+                count += 1
+
+        file, ext = re.split(r'\.', file_name)
+        return file + " ({}).".format(count) + ext if count > 1 else file_name
+
 
     @api.model
     def create(self, vals):
@@ -16,15 +30,17 @@ class IrAttachment(models.Model):
                 ('name', '=', attachment_id.res_name)])
             if not save_directory_id:
                 save_directory_id = self.env['dms.directory'].create({
-                    'name': attachment_id.res_name.replace('/','-'),
+                    'name': attachment_id.res_name.replace('/', '-'),
                     'parent_id': directory_id.id,
                     'storage_id': directory_id.root_storage_id.id,
                     'record_ref': "{},{}".format(
                         attachment_id.res_model, attachment_id.res_id),
                 })
 
+            file_name = self._dms_file_name(save_directory_id, vals['name'])
+
             self.env['dms.file'].create({
-                'name': vals['name'],
+                'name': file_name,
                 'directory_id': save_directory_id.id,
                 'attachment_id': attachment_id.id,
                 'record_ref': "{},{}".format(
@@ -32,8 +48,11 @@ class IrAttachment(models.Model):
             })
 
         elif directory_id:
+
+            file_name = self._dms_file_name(directory_id, vals['name'])
+
             self.env['dms.file'].create({
-                'name': vals['name'],
+                'name': file_name,
                 'directory_id': directory_id.id,
                 'attachment_id': attachment_id.id,
                 'record_ref': "{},{}".format(
