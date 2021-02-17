7880555a71131e88a6097ccd6c759c2f7c5d16f3
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-17 13:14:43 +0100

    [FIX] dms: Use sudo() in attachment operacion (search dms directories and auto-create directories and files if is needed) to prevent error in attachment(s) creation
    Co-authored-by: Jairo Llopis <jairo.llopis@tecnativa.com>

--- dms/tests/__init__.py
+++ dms/tests/__init__.py
@@ -6,4 +6,5 @@ from . import test_file_database
 from . import test_file
 from . import test_benchmark
 from . import test_directory_mail
+from . import test_ir_attachment
 from . import test_portal
