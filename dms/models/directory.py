17386d0bbf139ddeb2bb6920b8777afa74190ff1
Víctor Martínez <victor.martinez@tecnativa.com>
2021-04-08 08:19:28 +0100

    [IMP] dms: black, isort, prettier

--- dms/models/directory.py
+++ dms/models/directory.py
@@ -198,7 +198,9 @@ class DmsDirectory(models.Model):
     def _get_share_url(self, redirect=False, signup_partner=False, pid=None):
         self.ensure_one()
         return "/my/dms/directory/{}?access_token={}&db={}".format(
-            self.id, self._portal_ensure_token(), self.env.cr.dbname,
+            self.id,
+            self._portal_ensure_token(),
+            self.env.cr.dbname,
         )
 
     def check_access_token(self, access_token=False):
@@ -347,7 +349,8 @@ class DmsDirectory(models.Model):
         for category in self:
             if category.parent_id:
                 category.complete_name = "{} / {}".format(
-                    category.parent_id.complete_name, category.name,
+                    category.parent_id.complete_name,
+                    category.name,
                 )
             else:
                 category.complete_name = category.name
@@ -416,7 +419,8 @@ class DmsDirectory(models.Model):
                 record.size = 0
                 continue
             recs = sudo_model.search_read(
-                domain=[("directory_id", "child_of", record.id)], fields=["size"],
+                domain=[("directory_id", "child_of", record.id)],
+                fields=["size"],
             )
             record.size = sum(rec.get("size", 0) for rec in recs)
 
