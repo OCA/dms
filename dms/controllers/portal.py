c8cb967aab41f3105b40ca7ecc1817eee1c3e642
Víctor Martínez <victor.martinez@tecnativa.com>
2021-05-26 15:07:07 +0200

    [FIX] dms: Fix error in portal with access_token

--- dms/controllers/portal.py
+++ dms/controllers/portal.py
@@ -1,32 +1,24 @@
-# Copyright 2020 Tecnativa - Víctor Martínez
+# Copyright 2020-2021 Tecnativa - Víctor Martínez
 import base64
-import logging
 
 from odoo import _, http
-from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 from odoo.osv.expression import OR
 
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.addons.web.controllers.main import content_disposition, ensure_db
 
-_logger = logging.getLogger(__name__)
-
 
 class CustomerPortal(CustomerPortal):
     def _dms_check_access(self, model, res_id, access_token=None):
-        try:
-            item = request.env[model].browse(res_id)
-        except (AccessError, MissingError):
-            return False
-
         if access_token:
+            item = request.env[model].sudo().browse(res_id)
             if not item.check_access_token(access_token):
                 return False
         else:
+            item = request.env[model].browse(res_id)
             if not item.with_user(request.env.user.id).check_access("read", False):
                 return False
-
         return item
 
     def _prepare_portal_layout_values(self):
@@ -71,11 +63,7 @@ class CustomerPortal(CustomerPortal):
                 search_domain = OR([search_domain, [("name", "ilike", search)]])
             domain += search_domain
         # content according to pager and archive selected
-        items = (
-            request.env["dms.directory"]
-            .with_user(request.env.user.id)
-            .search(domain, order=sort_br)
-        )
+        items = request.env["dms.directory"].search(domain, order=sort_br)
         request.session["my_dms_folder_history"] = items.ids
         # values
         values.update(
@@ -131,11 +119,14 @@ class CustomerPortal(CustomerPortal):
                 search_domain = OR([search_domain, [("name", "ilike", search)]])
             domain += search_domain
         # content according to pager and archive selected
-        dms_directory_items = (
-            request.env["dms.directory"]
-            .with_user(request.env.user.id)
-            .search(domain, order=sort_br)
-        )
+        if access_token:
+            dms_directory_items = (
+                request.env["dms.directory"].sudo().search(domain, order=sort_br)
+            )
+        else:
+            dms_directory_items = request.env["dms.directory"].search(
+                domain, order=sort_br
+            )
         request.session["my_dms_folder_history"] = dms_directory_items.ids
         # check_access
         res = self._dms_check_access("dms.directory", dms_directory_id, access_token)
@@ -157,15 +148,16 @@ class CustomerPortal(CustomerPortal):
                 search_domain = OR([search_domain, [("name", "ilike", search)]])
             domain += search_domain
         # items
-        dms_file_items = (
-            request.env["dms.file"]
-            .with_user(request.env.user.id)
-            .search(domain, order=sort_br)
-        )
+        if access_token:
+            dms_file_items = (
+                request.env["dms.file"].sudo().search(domain, order=sort_br)
+            )
+        else:
+            dms_file_items = request.env["dms.file"].search(domain, order=sort_br)
         request.session["my_dms_file_history"] = dms_file_items.ids
-        dms_parent_categories = dms_directory_sudo.with_user(
-            request.env.user.id
-        )._get_parent_categories(access_token)
+        dms_parent_categories = dms_directory_sudo.sudo()._get_parent_categories(
+            access_token
+        )
         # values
         values = {
             "dms_directories": dms_directory_items,
@@ -201,6 +193,11 @@ class CustomerPortal(CustomerPortal):
                 return request.redirect("/my")
 
         dms_file_sudo = res
+        # It's necessary to prevent AccessError in ir_attachment .check() function
+        if dms_file_sudo.attachment_id and request.env.user.has_group(
+            "base.group_portal"
+        ):
+            dms_file_sudo = dms_file_sudo.sudo()
         filecontent = base64.b64decode(dms_file_sudo.content)
         content_type = ["Content-Type", "application/octet-stream"]
         disposition_content = [
