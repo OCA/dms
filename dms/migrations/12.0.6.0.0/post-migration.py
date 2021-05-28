e4b9a85b99baa1721e69cf8eac278c4d4c10150f
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-05-28 12:20:08 +0100

    [FIX] dms: image thumbnails performance
    - Portal was too slow to load because every thumbnail came base64-encoded. Instead, now it loads resources externally, making the initial request much faster, and benefiting from caching.
    - Fields `custom_thumbnail_medium` and `custom_thumbnail_small` are now stored, because non-stored thumbnails become performance bottlenecks easily.
    - Added field `icon_url` that allows using always the exact same image path for the exact same mime type. This saves a lot of requests and bandwidth.
    - Fields `thumbnail`, `thumbnail_medium` and `thumbnail_small` are now only used in forms, and do not perform resize operations.
    - Remove unused `size` parameter in thumbnail internal getters.
    - All thumbnail fields were getting resized to sizes that were different from the sizes that, later, were actually used in UI, thus making the UI always re-resize them. Removed that nonsense. Thumbnails now use the same size that the UI will require, and then UI needs to ask for no resizes.
    - Typo fixed in unknown file default thumbnail.
    - Do your customers really know what a mime type is? I bet not. Remove "type" column from portal. Thumbnails provide all the required information.
    
    @Tecnativa TT29847

---
+++ dms/migrations/12.0.6.0.0/post-migration.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Tecnativa - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env):
+    for model in ("dms.file", "dms.directory"):
+        _logger.info("Computing stored custom thumbnails for model %s", model)
+        records = env[model].search([("custom_thumbnail", "!=", False)])
+        records._compute_custom_thumbnail()
--- dms/models/mixins_thumbnail.py
+++ dms/models/mixins_thumbnail.py
@@ -14,73 +14,82 @@ class Thumbnail(models.AbstractModel):
     _name = "dms.mixins.thumbnail"
     _description = "Thumbnail Mixin"
 
-    custom_thumbnail = fields.Binary(
-        string="Custom Thumbnail", attachment=False, prefetch=False,
-    )
+    custom_thumbnail = fields.Binary(string="Custom Thumbnail", attachment=False)
     custom_thumbnail_medium = fields.Binary(
-        "Medium Custom Thumbnail", compute="_compute_custom_thumbnail", prefetch=False,
+        "Medium Custom Thumbnail",
+        attachment=True,
+        compute="_compute_custom_thumbnail",
+        store=True,
     )
     custom_thumbnail_small = fields.Binary(
-        "Small Custom Thumbnail", compute="_compute_custom_thumbnail", prefetch=False,
+        "Small Custom Thumbnail",
+        attachment=True,
+        compute="_compute_custom_thumbnail",
+        store=True,
     )
-
-    thumbnail = fields.Binary(
-        compute="_compute_thumbnail", string="Thumbnail", prefetch=False,
-    )
-
-    thumbnail_medium = fields.Binary(
-        "Medium Thumbnail", compute="_compute_thumbnail", prefetch=False,
-    )
-
-    thumbnail_small = fields.Binary(
-        "SmallThumbnail", compute="_compute_thumbnail", prefetch=False,
-    )
+    thumbnail = fields.Binary(compute="_compute_thumbnail", string="Thumbnail",)
+    thumbnail_medium = fields.Binary("Medium Thumbnail", compute="_compute_thumbnail")
+    thumbnail_small = fields.Binary("Small Thumbnail", compute="_compute_thumbnail")
+    icon_url = fields.Char("Icon URL", compute="_compute_icon_url")
 
     # ----------------------------------------------------------
     # Helper
     # ----------------------------------------------------------
 
     @api.model
-    def _get_thumbnail_placeholder_image(self, size, name):
-        path = self._get_thumbnail_path(size, name)
+    def _get_thumbnail_placeholder_image(self, name):
+        path = self._get_thumbnail_path(name)
         with open(path, "rb") as image:
             return base64.b64encode(image.read())
 
     @api.model
-    def _get_thumbnail_path(self, size, name):
+    def _get_thumbnail_path(self, name):
         folders = ["static", "lib", "img", "thumbnails"]
         path = get_resource_path("dms", *folders, name)
         if not os.path.isfile(path):
-            path = get_resource_path("dms", *folders, "file_unkown.png")
+            path = get_resource_path("dms", *folders, "file_unknown.png")
         return path
 
     def _get_thumbnail_placeholder_name(self):
         return "folder.png"
 
-    @api.depends("custom_thumbnail")
-    def _compute_custom_thumbnail(self):
+    def _compute_icon_url(self):
+        """Get icon static file URL."""
+        for one in self:
+            thumbnail_path = one._get_thumbnail_path(
+                one._get_thumbnail_placeholder_name()
+            )
+            thumbnail_name = os.path.basename(thumbnail_path)
+            one.icon_url = "/dms/static/lib/img/thumbnails/%s" % thumbnail_name
+
+    @api.depends("custom_thumbnail_medium", "custom_thumbnail_small")
+    def _compute_thumbnail(self):
+        """Generate small thumbnails, with icon fallback.
+
+        NEVER use ``thumbnail_*`` fields in list/kanban views, or things will
+        slow down. Instead, use directly ``icon_url`` or ``custom_thumbnail_*``.
+
+        It reflects ``custom_thumbnail_*`` fields if possible, but defaults to
+        the mimetype icon.
+        """
         for record in self:
-            if not record.custom_thumbnail:
-                record.custom_thumbnail_medium = False
-                record.custom_thumbnail_small = False
-            else:
-                resized_images = tools.image_get_resized_images(
-                    record.custom_thumbnail, return_big=True, avoid_resize_medium=True
-                )
-                record.custom_thumbnail_medium = resized_images["image_medium"]
-                record.custom_thumbnail_small = resized_images["image_small"]
+            icon_fallback = record._get_thumbnail_placeholder_image(
+                record._get_thumbnail_placeholder_name()
+            )
+            for field in ("thumbnail", "thumbnail_medium", "thumbnail_small"):
+                record[field] = record["custom_%s" % field] or icon_fallback
 
     @api.depends("custom_thumbnail")
-    def _compute_thumbnail(self):
+    def _compute_custom_thumbnail(self):
+        """Generate small custom thumbnails.
+
+        They will be empty if the main custom thumbnail is empty.
+        """
         for record in self:
-            if record.custom_thumbnail:
-                record.thumbnail = record.with_context(bin_size=False).custom_thumbnail
-            else:
-                record.thumbnail = self._get_thumbnail_placeholder_image(
-                    "original", record._get_thumbnail_placeholder_name()
+            record.update(
+                tools.image_get_resized_images(
+                    record.with_context(bin_size=False).custom_thumbnail,
+                    medium_name="custom_thumbnail_medium",
+                    small_name="custom_thumbnail_small",
                 )
-            resized_images = tools.image_get_resized_images(
-                record.thumbnail, return_big=True, avoid_resize_medium=True
             )
-            record.thumbnail_medium = resized_images["image_medium"]
-            record.thumbnail_small = resized_images["image_small"]
