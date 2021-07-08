d8a94b387ffbcc984009030f1eedc00a4c30a684
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-08 12:35:09 +0200

    [FIX] dms: Fix multi-company rules + migration scripts

---
+++ dms/migrations/14.0.2.1.0/post-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "dms", "migrations/14.0.2.1.0/noupdate_changes.xml")
