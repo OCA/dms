0cd575ca55ddf6399bb72480a3f7d6aa4f3232cf
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-14 16:44:09 +0200

    [IMP] dms: Show in directory kaban view only root directories (Change All text to Root) + Change to searchpanel directory_id (in files) or parent_id (in directories) to filter equal and not child_of

---
+++ dms/static/src/js/views/search_panel.js
@@ -0,0 +1,45 @@
+/* Copyright 2021 Tecnativa - Víctor Martínez
+ * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+odoo.define("dms.SearchPanel", function(require) {
+    "use strict";
+
+    const core = require("web.core");
+    const SearchPanel = require("web.SearchPanel");
+    const _t = core._t;
+    SearchPanel.include({
+        _renderCategory: function() {
+            let res = this._super.apply(this, arguments);
+            if (this.model === "dms.directory") {
+                // Replace label on JS because QWeb has not enough context
+                const res_dom = $(res);
+                res_dom.find(".o_search_panel_label_title b").html(_t("Root"));
+                res = res_dom.html();
+            }
+            return res;
+        },
+        _getCategoryDomain: function() {
+            var domain = this._super.apply(this, arguments);
+            var field_name_need_check = false;
+            if (this.model === "dms.file") {
+                field_name_need_check = "directory_id";
+            } else if (this.model === "dms.directory") {
+                field_name_need_check = "parent_id";
+            }
+            if (field_name_need_check) {
+                domain.forEach(function(item, key) {
+                    if (item[0] === field_name_need_check && item[1] === "child_of") {
+                        domain[key] = [item[0], "=", item[2]];
+                    }
+                });
+            }
+            if (
+                domain.length === 0 &&
+                field_name_need_check &&
+                this.model === "dms.directory"
+            ) {
+                domain.push([field_name_need_check, "=", false]);
+            }
+            return domain;
+        },
+    });
+});
