7d582ec705830556df6ed2a150ad5aadb044f4d2
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-04-16 19:38:37 -0300

    [RFC] dms_field: change license from agpl to lgpl Approved Enric Tobella https://github.com/OCA/dms/issues/169

--- dms_field/__manifest__.py
+++ dms_field/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2020 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "DMS Field",
     "summary": """
         Create DMS View and allow to use them inside a record""",
     "version": "14.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/dms",
     "depends": ["dms"],
