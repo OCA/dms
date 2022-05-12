ab21563667ccab9bc7219598fa6b791dd80d6e05
gtorresemmanuel <47044159+gtorresemmanuel@users.noreply.github.com>
2022-05-12 18:48:55 -0500

    Update post-migration.py

--- dms/migrations/14.0.4.0.0/post-migration.py
+++ dms/migrations/14.0.4.0.0/post-migration.py
@@ -5,12 +5,12 @@ from openupgradelib import openupgrade, openupgrade_90
 
 def convert_binary_fields_to_attachment(env):
     """Convert old user-provided thumbnails to attachments."""
-    column = openupgrade.get_legacy_name("custom_thumbnail")
-    spec = {
-        "dms.file": [("image_1920", column)],
-        "dms.directory": [("image_1920", column)],
-    }
-    openupgrade_90.convert_binary_field_to_attachment(env, spec)
+    #column = openupgrade.get_legacy_name("custom_thumbnail")
+    #spec = {
+    #    "dms.file": [("image_1920", column)],
+    #    "dms.directory": [("image_1920", column)],
+    #}
+    #openupgrade_90.convert_binary_field_to_attachment(env, spec)
     # Force rewrite to force resizing and recomputing smaller thumbnails
     env["dms.file"].search([])._compute_image_1920()
     for record in env["dms.file"].search(
