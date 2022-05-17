3e8aed2587cacfa121f74f619337cda8c70b8ec0
Víctor Martínez <victor.martinez@tecnativa.com>
2022-05-17 13:25:09 +0200

    [IMP] dms_version: Change to AGPL according to base_revision. TT29907

--- dms_version/__manifest__.py
+++ dms_version/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2017-2020 MuK IT GmbH
 # Copyright 2021 Tecnativa - Víctor Martínez
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Document Management System Version",
     "summary": """Version Control for Documents""",
     "version": "14.0.1.0.0",
     "category": "Document Management",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "https://github.com/OCA/dms",
     "author": "MuK IT, Tecnativa, Odoo Community Association (OCA)",
     "depends": ["dms", "base_revision"],
--- dms_version/models/__init__.py
+++ dms_version/models/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 MuK IT GmbH
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import dms_file
 from . import dms_storage
