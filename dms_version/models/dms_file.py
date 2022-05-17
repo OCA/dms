78fdb391756c73067c519e1ccf78a01666a64372
Víctor Martínez <victor.martinez@tecnativa.com>
2022-05-17 13:25:09 +0200

    [IMP] dms_version: Change to AGPL according to base_revision. TT29907

--- dms_version/models/dms_file.py
+++ dms_version/models/dms_file.py
@@ -1,6 +1,6 @@
 # Copyright 2017-2020 MuK IT GmbH
 # Copyright 2021 Tecnativa - Víctor Martínez
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
--- dms_version/tests/__init__.py
+++ dms_version/tests/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2017-2020 MuK IT GmbH
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_dms_file
