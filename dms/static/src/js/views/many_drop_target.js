21b9299435541a1ea922aea913a8826a73c0ee80
Giedrius Slavinskas <giedrius@inovera.lt>
2022-07-15 17:32:30 +0300

    [FIX] dms: do_warn method no longer available in v15

--- dms/static/src/js/views/many_drop_target.js
+++ dms/static/src/js/views/many_drop_target.js
@@ -47,7 +47,10 @@ odoo.define("dms.DragDrop", function (require) {
             }
             console.log(ctx);
             if (typeof ctx.default_directory_id === "undefined") {
-                return this.do_warn(_t("You must select a directory first"));
+                return this.displayNotification({
+                    message: _t("You must select a directory first"),
+                    type: "danger",
+                });
             }
             return this._rpc({
                 model: res_model,
