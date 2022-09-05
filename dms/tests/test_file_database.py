abfa64533d83903fd1e94af077cf1971a5df8789
Víctor Martínez <victor.martinez@tecnativa.com>
2022-09-05 13:48:54 +0200

    [IMP] dms: Refactor tests.

--- dms/tests/test_file_database.py
+++ dms/tests/test_file_database.py
@@ -1,135 +1,120 @@
 # Copyright 2017-2019 MuK IT GmbH.
 # Copyright 2020 Creu Blanca
-# Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021-2022 Tecnativa - Víctor Martínez
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.exceptions import UserError
+from odoo.tests.common import users
 
-from .common import DocumentsBaseCase, multi_users
+from .common import StorageDatabaseBaseCase
 
 
-class FileTestCase(DocumentsBaseCase):
-    def _setup_test_data(self):
-        super(FileTestCase, self)._setup_test_data()
-        self.directory_root_demo_01 = self.browse_ref("dms.directory_01_demo")
-        self.directory_root_demo_02 = self.browse_ref("dms.directory_02_demo")
-        self.directory_root_demo_03 = self.browse_ref("dms.directory_03_demo")
-        self.directory_sub_demo_01 = self.browse_ref("dms.directory_11_demo")
-        self.directory_sub_demo_02 = self.browse_ref("dms.directory_12_demo")
-        self.file_demo_01 = self.browse_ref("dms.file_01_demo")
-        self.file_demo_02 = self.browse_ref("dms.file_02_demo")
-        self.file_demo_03 = self.browse_ref("dms.file_03_demo")
-        self.file_demo_04 = self.browse_ref("dms.file_04_demo")
-        self.file_demo_05 = self.browse_ref("dms.file_05_demo")
-        self.new_storage = self.create_storage(sudo=True)
-        self.new_root_directory = self.create_directory(storage=self.new_storage)
-        self.new_sub_directory = self.create_directory(
-            directory=self.new_root_directory
-        )
-        self.new_file_root_directory = self.create_file(
-            directory=self.new_root_directory
-        )
-        self.new_file_sub_directory = self.create_file(directory=self.new_sub_directory)
+class FileDatabaseTestCase(StorageDatabaseBaseCase):
+    def setUp(self):
+        super().setUp()
+        self.file_demo_01 = self.env.ref("dms.file_01_demo")
+        self.directory2 = self.create_directory(storage=self.storage)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_create_file(self):
-        root_directory = self.create_directory(storage=self.new_storage)
+        root_directory = self.create_directory(storage=self.storage)
         self.create_file(directory=root_directory)
         sub_directory = self.create_directory(directory=root_directory)
         self.create_file(sub_directory)
         self.assertEqual(root_directory.count_total_files, 2)
         self.assertEqual(sub_directory.count_files, 1)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_lock_file(self):
-        file = self.create_file(sudo=True)
-        file.with_user(self.env.uid).lock()
+        file = self.create_file(directory=self.directory)
+        file.lock()
         self.assertTrue(file.is_locked)
-        file.with_user(self.env.uid).unlock()
+        file.unlock()
         self.assertFalse(file.is_locked)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_copy_file(self):
-        copy_file = self.file_demo_01.copy()
-        self.assertEqual(self.file_demo_01.storage_id, copy_file.storage_id)
-        self.assertEqual(self.file_demo_01.content, copy_file.content)
+        copy_file = self.file.copy()
+        self.assertEqual(self.file.storage_id, copy_file.storage_id)
+        self.assertEqual(self.file.content, copy_file.content)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_rename_file(self):
-        file = self.create_file(sudo=True)
+        file = self.create_file(directory=self.directory)
         extension = file.extension
-        file.with_user(self.env.uid).write({"name": "Test.jpg"})
+        file.write({"name": "test-%s.jpg" % self.env.user.login})
         self.assertNotEqual(file.extension, extension)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_move_file(self):
-        path_names = self.file_demo_01.path_names
-        self.file_demo_01.write({"directory_id": self.directory_root_demo_01.id})
-        self.file_demo_01.flush()
-        self.assertNotEqual(path_names, self.file_demo_01.path_names)
+        file = self.create_file(directory=self.directory)
+        path_names = file.path_names
+        file.write({"directory_id": self.directory2.id})
+        file.flush()
+        self.assertNotEqual(path_names, file.path_names)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_move_directory(self):
         with self.assertRaises(UserError):
-            self.directory_root_demo_01.write(
+            self.directory.write(
                 {
                     "is_root_directory": False,
-                    "parent_id": self.directory_root_demo_02.id,
+                    "parent_id": self.directory2.id,
                 }
             )
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_unlink_file(self):
-        file = self.create_file(sudo=True)
-        file.with_user(self.env.uid).unlink()
+        file = self.create_file(directory=self.directory)
+        file.unlink()
         self.assertFalse(file.exists())
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_thumbnail(self):
         self.assertTrue(self.file_demo_01.image_128)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_path_names(self):
-        self.assertTrue(self.file_demo_01.path_names)
+        self.assertTrue(self.file.path_names)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_path_json(self):
-        self.assertTrue(self.file_demo_01.path_json)
+        self.assertTrue(self.file.path_json)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_mimetype(self):
-        self.assertTrue(self.file_demo_01.mimetype)
+        self.assertTrue(self.file.mimetype)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_extension(self):
-        self.assertTrue(self.file_demo_01.extension)
+        self.assertTrue(self.file.extension)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_size_calculation(self):
-        self.assertTrue(self.file_demo_01.size)
+        self.assertTrue(self.file.size)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_checksum_calculation(self):
-        self.assertTrue(self.file_demo_01.checksum)
+        self.assertTrue(self.file.checksum)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_content(self):
-        self.assertTrue(self.file_demo_01.with_context(bin_size=True).content)
-        self.assertTrue(self.file_demo_01.with_context(bin_size=False).content)
+        self.assertTrue(self.file.with_context(bin_size=True).content)
+        self.assertTrue(self.file.with_context(bin_size=False).content)
         self.assertNotEqual(
-            self.file_demo_01.with_context(bin_size=False).content,
-            self.file_demo_01.with_context(bin_size=True).content,
+            self.file.with_context(bin_size=False).content,
+            self.file.with_context(bin_size=True).content,
         )
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_save_type(self):
-        self.assertTrue(self.file_demo_01.save_type)
+        self.assertTrue(self.file.save_type)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_compute_migration(self):
-        self.assertTrue(self.file_demo_01.migration)
+        self.assertTrue(self.file.migration)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_search_panel(self):
         self.assertTrue(self.file.search_panel_select_range("directory_id"))
         self.assertTrue(self.file.search_panel_select_multi_range("directory_id"))
--- dms/tests/test_storage_database.py
+++ dms/tests/test_storage_database.py
@@ -1,22 +1,82 @@
 # Copyright 2017-2019 MuK IT GmbH.
 # Copyright 2020 Creu Blanca
+# Copyright 2022 Víctor Martínez
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from .common import DocumentsBaseCase, multi_users
+from odoo.tests.common import users
+
+from .common import StorageDatabaseBaseCase
 
 
-class StorageTestCase(DocumentsBaseCase):
-    def _setup_test_data(self):
-        self.storage_demo = self.browse_ref("dms.storage_demo")
-
-    @multi_users(lambda self: self.multi_users(demo=False), callback="_setup_test_data")
+class StorageDatabaseTestCase(StorageDatabaseBaseCase):
+    @users("dms-manager")
     def test_action_storage_migrate(self):
-        self.storage_demo.with_user(self.uid).action_storage_migrate()
+        self.storage.action_storage_migrate()
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_count_storage_directories(self):
-        self.assertTrue(self.storage_demo.count_storage_directories)
+        self.assertTrue(self.storage.count_storage_directories)
 
-    @multi_users(lambda self: self.multi_users(), callback="_setup_test_data")
+    @users("dms-manager", "dms-user")
     def test_count_storage_files(self):
-        self.assertTrue(self.storage_demo.count_storage_files)
+        self.assertTrue(self.storage.count_storage_files)
+
+    @users("dms-manager")
+    def test_file_migrate(self):
+        root_directory = self.storage.root_directory_ids.filtered(
+            lambda x: x.is_root_directory
+        )
+        file_01 = self.create_file(directory=root_directory)
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "database")
+        self.assertEqual(file_01.save_type, "database")
+        self.storage.write({"save_type": "file"})
+        directory = self.create_directory(storage=self.storage)
+        file_02 = self.create_file(directory=directory)
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "file")
+        self.assertEqual(file_01.save_type, "database")
+        self.assertEqual(file_02.storage_id, self.storage)
+        self.assertEqual(file_02.storage_id.save_type, "file")
+        self.assertEqual(file_02.save_type, "file")
+        self.storage.action_storage_migrate()
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "file")
+        self.assertEqual(file_01.save_type, "file")
+        self.assertEqual(file_02.storage_id, self.storage)
+        self.assertEqual(file_02.storage_id.save_type, "file")
+        self.assertEqual(file_02.save_type, "file")
+        self.storage.write({"save_type": "database"})
+        self.storage.action_storage_migrate()
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "database")
+        self.assertEqual(file_01.save_type, "database")
+        self.assertEqual(file_02.storage_id, self.storage)
+        self.assertEqual(file_02.storage_id.save_type, "database")
+        self.assertEqual(file_02.save_type, "database")
+        self.storage.write({"save_type": "attachment"})
+        model_partner = self.env.ref("base.model_res_partner")
+        partner = self.partner_model.create({"name": "Test partner"})
+        directory = self.directory_model.create(
+            {
+                "name": model_partner.model,
+                "model_id": model_partner.id,
+                "res_id": partner.id,
+                "parent_id": self.storage.root_directory_ids[0].id,
+                "storage_id": self.storage.id,
+            }
+        )
+        file_03 = self.create_file(directory=directory)
+        self.assertEqual(file_02.storage_id, self.storage)
+        self.assertEqual(file_02.storage_id.save_type, "attachment")
+        self.assertEqual(file_02.save_type, "database")
+        self.assertEqual(file_03.storage_id, self.storage)
+        self.assertEqual(file_03.storage_id.save_type, "attachment")
+        self.assertEqual(file_03.save_type, "database")
+        self.storage.action_storage_migrate()
+        self.assertEqual(file_02.storage_id, self.storage)
+        self.assertEqual(file_02.storage_id.save_type, "attachment")
+        self.assertEqual(file_02.save_type, "database")
+        self.assertEqual(file_03.storage_id, self.storage)
+        self.assertEqual(file_03.storage_id.save_type, "attachment")
+        self.assertEqual(file_03.save_type, "database")
