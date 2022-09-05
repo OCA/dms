8e60457658e5972af95dbb5537eeef13eb5efca6
Víctor Martínez <victor.martinez@tecnativa.com>
2022-09-05 13:48:54 +0200

    [IMP] dms: Refactor tests.

--- dms/tests/test_storage_attachment.py
+++ dms/tests/test_storage_attachment.py
@@ -1,121 +1,83 @@
-# Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021-2022 Tecnativa - Víctor Martínez
 # Copyright 2021 Tecnativa - João Marques
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from .common import DocumentsBaseCase
+from odoo.tests.common import users
+
+from .common import StorageAttachmentBaseCase
 
 
-class StorageAttachmentTestCase(DocumentsBaseCase):
-    def setUp(self):
-        super().setUp()
-        self.storage = self.browse_ref("dms.storage_attachment_demo")
-        self.model_res_partner = self.browse_ref("base.model_res_partner")
-        self.partner = self.env["res.partner"].create({"name": "test partner"})
-        self.user = self.env["res.users"].create(
-            {
-                "name": "name",
-                "login": "login",
-                "groups_id": [(6, 0, [self.env.ref("base.group_user").id])],
-            }
-        )
-        self.user_demo = self.env["res.users"].create(
-            {
-                "name": "User Base",
-                "login": "user_demo",
-                "groups_id": [
-                    (
-                        6,
-                        0,
-                        [
-                            self.env.ref("base.group_partner_manager").id,
-                            self.env.ref("base.group_user").id,
-                            self.env.ref("base.group_no_one").id,
-                            self.env.ref("dms.group_dms_user").id,
-                        ],
-                    )
-                ],
-            }
-        )
-        self.user_admin = self.env["res.users"].create(
-            {
-                "name": "User Admin",
-                "login": "user_admin",
-                "groups_id": [
-                    (
-                        6,
-                        0,
-                        [
-                            self.env.ref("base.group_erp_manager").id,
-                            self.env.ref("base.group_partner_manager").id,
-                            self.env.ref("base.group_user").id,
-                            self.env.ref("dms.group_dms_manager").id,
-                            self.env.ref("base.group_system").id,
-                            self.env.ref("base.group_no_one").id,
-                            self.env.ref("dms.group_dms_user").id,
-                        ],
-                    )
-                ],
-            }
-        )
-
-    def _create_attachment(self, name, uid):
-        self.create_attachment(
-            name=name,
-            res_model=self.model_res_partner.model,
-            res_id=self.partner.id,
-            sudo=False,
-        ).with_user(uid)
-
+class StorageAttachmentTestCase(StorageAttachmentBaseCase):
+    @users("dms-manager")
     def test_storage_attachment(self):
-        self._create_attachment("demo.txt", self.user_admin.id)
+        self._create_attachment("demo.txt")
         self.assertTrue(
             self.storage.storage_file_ids.filtered(lambda x: x.name == "demo.txt")
         )
-        directory_id = self.directory.with_user(self.user_admin.id).search(
-            [
-                ("storage_id", "=", self.storage.id),
-                ("res_model", "=", self.model_res_partner.model),
-                ("res_id", "=", self.partner.id),
-            ]
-        )
-        self.assertEqual(directory_id.count_files, 1)
-        self.assertEqual(directory_id.file_ids[0].name, "demo.txt")
-        file_01 = self.create_file(
-            directory=directory_id,
-            storage=directory_id.storage_id,
-        ).with_user(self.user_admin.id)
+        directory = self._get_partner_directory()
+        self.assertEqual(directory.count_files, 1)
+        self.assertEqual(directory.file_ids[0].name, "demo.txt")
+        file_01 = self.create_file(directory=directory)
         self.assertEqual(file_01.storage_id, self.storage)
         self.assertEqual(file_01.storage_id.save_type, "attachment")
         self.assertEqual(file_01.save_type, "database")
-        self.assertEqual(directory_id.count_files, 2)
+        self.assertEqual(directory.count_files, 2)
         # Assert cascade removal
         self.partner.unlink()
         self.assertFalse(file_01.exists())
-        self.assertFalse(directory_id.exists())
+        self.assertFalse(directory.exists())
 
-    def test_storage_attachment_directory_record_ref_access(self):
-        self._create_attachment("demo.txt", self.user_admin.id)
-        directory_id = self.directory.with_user(self.user_admin.id).search(
-            [
-                ("storage_id", "=", self.storage.id),
-                ("res_model", "=", self.model_res_partner.model),
-                ("res_id", "=", self.partner.id),
-            ]
+    @users("dms-manager")
+    def test_storage_attachment_misc(self):
+        attachment = self._create_attachment("demo.txt")
+        root_directory = self.storage.root_directory_ids.filtered(
+            lambda x: x.is_root_directory
         )
-        self.assertTrue(directory_id.with_user(self.user_admin.id).permission_read)
-        self.assertTrue(directory_id.with_user(self.user_demo.id).permission_read)
-        self.assertTrue(directory_id.with_user(self.user.id).permission_read)
+        self.assertEqual(root_directory.count_directories, 1)
+        self.assertEqual(len(root_directory.child_directory_ids), 1)
+        child_directory = root_directory.child_directory_ids[0]
+        self.assertEqual(child_directory.res_model, self.partner_model._name)
+        self.assertEqual(child_directory.res_id, self.partner.id)
+        self.assertEqual(len(root_directory.child_directory_ids.file_ids), 1)
+        dms_file = root_directory.child_directory_ids.file_ids[0]
+        self.assertEqual(dms_file.attachment_id, attachment)
+        self.assertEqual(dms_file.name, "demo.txt")
+        self.assertEqual(dms_file.res_model, self.partner_model._name)
+        self.assertEqual(dms_file.res_id, self.partner.id)
+        directory = self._get_partner_directory()
+        file_01 = self.create_file(directory=directory)
+        self.assertEqual(file_01.res_model, self.partner_model._name)
+        self.assertEqual(file_01.res_id, self.partner.id)
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "attachment")
+        self.assertEqual(file_01.save_type, "database")
+        self.assertEqual(self.storage.count_storage_files, 2)
+
+    @users("dms-manager")
+    def test_storage_attachment_directory_record_ref_access_dms_manager(self):
+        self._create_attachment("demo.txt")
+        directory = self._get_partner_directory()
+        self.assertTrue(directory.permission_read)
         self.assertEqual(self.partner.type, "contact")
         self.partner.sudo().write({"type": "private"})
         self.assertEqual(self.partner.type, "private")
-        self.assertTrue(directory_id.sudo().permission_read)
-        directory_id.invalidate_cache()
-        self.assertFalse(directory_id.with_user(self.user_admin.id).permission_read)
-        directory_id.invalidate_cache()
-        self.assertFalse(directory_id.with_user(self.user_demo.id).permission_read)
-        self.assertFalse(directory_id.with_user(self.user.id).permission_read)
+        self.assertTrue(directory.sudo().permission_read)
+        directory.invalidate_cache()
+        self.assertFalse(directory.with_user(self.dms_manager_user).permission_read)
+
+    @users("dms-user")
+    def test_storage_attachment_directory_record_ref_access_dms_user(self):
+        self._create_attachment("demo.txt")
+        directory = self._get_partner_directory()
+        self.assertTrue(directory.permission_read)
+        self.assertEqual(self.partner.type, "contact")
+        self.partner.sudo().write({"type": "private"})
+        self.assertEqual(self.partner.type, "private")
+        self.assertTrue(directory.sudo().permission_read)
+        directory.invalidate_cache()
+        self.assertFalse(directory.with_user(self.dms_user).permission_read)
         # user can access self.partner
-        self.user_demo.write(
+        self.dms_user.write(
             {
                 "groups_id": [
                     (
@@ -129,4 +91,23 @@ class StorageAttachmentTestCase(DocumentsBaseCase):
                 ]
             }
         )
-        self.assertTrue(directory_id.with_user(self.user_demo.id).permission_read)
+        self.assertTrue(directory.permission_read)
+
+    @users("basic-user")
+    def test_storage_attachment_directory_record_ref_access_basic_user(self):
+        self._create_attachment("demo.txt")
+        directory = self._get_partner_directory()
+        self.assertTrue(directory.permission_read)
+        self.assertTrue(directory.permission_read)
+        self.assertEqual(self.partner.type, "contact")
+        self.partner.sudo().write({"type": "private"})
+        self.assertEqual(self.partner.type, "private")
+        self.assertTrue(directory.sudo().permission_read)
+        directory.invalidate_cache()
+        self.assertFalse(directory.with_user(self.user).permission_read)
+
+    @users("basic-user")
+    def test_create_attachment_basic_user(self):
+        attachment = self._create_attachment("Test file")
+        self.assertEqual(attachment.name, "Test file")
+        self.assertTrue(self._get_partner_directory())
