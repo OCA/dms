a0cc1c1fc004402ca74d99c487c141d6f7336669
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-08 17:24:35 +0100

    [ADD] dms_attachment_link: New module
    TT41512
    
    [UPD] Update dms_attachment_link.pot
    
    [UPD] README.rst

---
+++ dms_attachment_link/static/src/js/dms_attachment_link.js
@@ -0,0 +1,24 @@
+odoo.define("dms_attachment_link", function (require) {
+    "use strict";
+
+    var AttachmentBox = require("mail.AttachmentBox");
+
+    AttachmentBox.include({
+        events: _.extend(AttachmentBox.prototype.events, {
+            "click span.o_add_dms_file_button": "_onAddDmsFile",
+        }),
+        _onAddDmsFile: function () {
+            this.do_action("dms_attachment_link.action_wizard_ir_attachment_dms_file", {
+                additional_context: {
+                    active_id: this.currentResID,
+                    active_ids: [this.currentResID],
+                    active_model: this.currentResModel,
+                },
+                on_close: this._onAddedUrl.bind(this),
+            });
+        },
+        _onAddedUrl: function () {
+            this.trigger_up("reload_attachment_box");
+        },
+    });
+});
