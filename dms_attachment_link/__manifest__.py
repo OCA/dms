bfd94397d08afa34b0b9556d0fb88c29102833e1
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-10 09:53:42 +0100

    [IMP] dms_attachment_link: Add searchpanel to wizard to improve file selection.
    TT41512
    
    [UPD] Update dms_attachment_link.pot
    
    dms_attachment_link 13.0.1.1.0
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: dms-13.0/dms-13.0-dms_attachment_link
    Translate-URL: https://translation.odoo-community.org/projects/dms-13-0/dms-13-0-dms_attachment_link/

--- dms_attachment_link/__manifest__.py
+++ dms_attachment_link/__manifest__.py
@@ -2,13 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dms Attachment Link",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Document Management",
     "website": "https://github.com/OCA/dms",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["dms"],
-    "data": ["views/assets.xml", "wizard/wizard_ir_attachment_dms_file_view.xml"],
+    "data": [
+        "views/assets.xml",
+        "views/dms_file_views.xml",
+    ],
     "qweb": ["static/src/xml/chatter.xml"],
     "installable": True,
     "maintainers": ["victoralmau"],
