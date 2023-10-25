81e9e02b826da6f62561426d6e49264bcf5fe956
Enric Tobella <etobella@creublanca.es>
2023-10-25 15:20:45 +0200

    [FIX] dms: Improve search by model

--- dms/__manifest__.py
+++ dms/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Document Management System",
     "summary": """Document Management System for Odoo""",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.1.1",
     "category": "Document Management",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/dms",
