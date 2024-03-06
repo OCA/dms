72fe346348fb1dc26a112be71d1d8b2eb925079b
Víctor Martínez <victor.martinez@tecnativa.com>
2024-03-06 09:23:14 +0100

    [IMP] dms_field: Correctly sort the records (directories and files) in view.
    TT48181

--- dms_field/__manifest__.py
+++ dms_field/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "DMS Field",
     "summary": """
         Create DMS View and allow to use them inside a record""",
-    "version": "15.0.1.1.1",
+    "version": "15.0.1.1.2",
     "license": "LGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/dms",
