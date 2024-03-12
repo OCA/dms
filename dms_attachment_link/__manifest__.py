395f4357a97f0ce4385c7aded4d588161f1fc0ec
Demchuk Mykola <kolya233445@gmail.com>
2024-03-13 00:34:48 +0200

    [MIG] dms_attachment_link: Migration to 16.0
    Co-authored-by: DemchukM

--- dms_attachment_link/__manifest__.py
+++ dms_attachment_link/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dms Attachment Link",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Document Management",
     "website": "https://github.com/OCA/dms",
     "author": "Tecnativa, Odoo Community Association (OCA)",
@@ -14,8 +14,6 @@
     "assets": {
         "web.assets_backend": [
             "dms_attachment_link/static/src/js/dms_attachment_link.esm.js",
-        ],
-        "web.assets_qweb": [
             "dms_attachment_link/static/src/xml/chatter.xml",
         ],
     },
