255a821dce34c6a4fa5d30acc1b86fdacf881d1a
Georg A. G. Notter <georg.notter@agenterp.com>
2024-10-16 08:26:32 +0200

    [ADD] sale_dms_field: New module

---
+++ sale_dms_field/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Add dms field for sale",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "website": "https://github.com/OCA/dms",
+    "author": "Agent ERP GmbH,Odoo Community Association (OCA)",
+    "depends": ["sale_management", "dms_field"],
+    "data": ["views/sale_order_view.xml"],
+    "demo": ["demo/sale_dms_data.xml"],
+    "installable": True,
+    "license": "LGPL-3",
+}
