9da1aa04aeb9e3f274d18beaf727382affc01258
Enric Tobella <enric.tobella@dixmit.com>
2025-10-03 08:38:02 +0200

    [IMP] dms_field: Use ir.assets for xml to add the sequence and avoid race condition with hr_holidays accrual plan

--- dms_field/__manifest__.py
+++ dms_field/__manifest__.py
@@ -17,16 +17,12 @@
         "views/dms_storage.xml",
         "security/ir.model.access.csv",
         "security/security.xml",
+        "data/assets.xml",
     ],
     "assets": {
         "web.assets_backend": [
             "dms_field/static/src/**/*",
             ("remove", "dms_field/static/src/views/fields/x2many/x2many_field.xml"),
-            (
-                "after",
-                "/web/static/src/views/fields/x2many/x2many_field.xml",
-                "dms_field/static/src/views/fields/x2many/x2many_field.xml",
-            ),
         ],
     },
     "demo": ["demo/partner_dms.xml"],
