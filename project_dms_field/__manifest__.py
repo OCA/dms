3874313fc653c82ba4096f83eedc7203bc871bb1
Keith Brandenburg <windweaver828@gmail.com>
2026-06-03 16:40:18 -0500

    [ADD] project_dms_field: add DMS field for projects

---
+++ project_dms_field/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Windweaver
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Add dms field for projects",
+    "version": "18.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/dms",
+    "author": "Windweaver, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project", "dms_field"],
+    "installable": True,
+    "data": ["views/project_project_views.xml"],
+    "demo": ["demo/dms_data.xml"],
+    "maintainers": ["windweaver828"],
+}
---
+++ project_dms_field/readme/USAGE.md
@@ -0,0 +1,4 @@
+1. Go to the form view of an existing project and click on the *Documents* tab
+   icon. A hierarchy of folders and files linked to that project will be created.
+2. Create a new project. A hierarchy of folders and files linked to that project
+   will be created.
